bf258172f9dcf8e2a5283bb6bbefc1af634fa42d
RutamBhagat <rutambhagat@gmail.com>
2024-12-20 08:14:42 -0800

    feat(layout): update grid structure for consistent height across pages

--- src/app/repository/guide/page.tsx
+++ src/app/repository/guide/page.tsx
@@ -11,12 +11,11 @@ export default async function page() {
 
   return (
     <>
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+      <div className="grid min-h-[calc(100vh-4rem)] auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
     </>
   );
 }
--- src/app/repository/page.tsx
+++ src/app/repository/page.tsx
@@ -11,7 +11,7 @@ export default async function page() {
 
   return (
     <>
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+      <div className="grid min-h-[calc(100vh-4rem)] auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
